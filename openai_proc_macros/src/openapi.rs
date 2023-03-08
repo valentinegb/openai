54e568171602cccec4296bcaa1146ebddc5374f6
Valentine <35977727+valentinegb@users.noreply.github.com>
2023-03-08 07:56:26 -0800

    Begin work on generating from schema

---
+++ openai_proc_macros/src/openapi.rs
@@ -0,0 +1,45 @@
+// These structures are for the explicit purpose of deserializing the OpenAI API schema, not any OpenAPI schema.
+// I'm making my own structures and such instead of using a pre-existing library because
+// they're a bit overcomplicated for what they're needed for.
+
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Deserialize, Debug)]
+pub struct OpenApi {
+    pub servers: Vec<Server>,
+    pub components: Components,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Server {
+    pub url: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Components {
+    pub schemas: HashMap<String, Schema>,
+}
+
+// TODO: find out why schemas are failing to deserialze >:(
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum Schema {
+    Tagged(TaggedSchema),
+    Object { properties: HashMap<String, Schema> },
+    Other,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+pub enum TaggedSchema {
+    Null,
+    Boolean,
+    Object { properties: HashMap<String, Schema> },
+    Array,
+    Number,
+    String,
+    Integer,
+}
