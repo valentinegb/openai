370646648c6a187a3add08cb74a0ef12c8048ca8
Valentine <35977727+valentinegb@users.noreply.github.com>
2023-03-14 10:22:28 -0700

    Where I'm at right now

--- openai_proc_macros/src/lib.rs
+++ openai_proc_macros/src/lib.rs
@@ -1,26 +1,57 @@
 mod openapi;
 
 extern crate proc_macro;
-use openapi::OpenApi;
+use openapi::*;
 use proc_macro::TokenStream;
-use std::fs;
+use std::{collections::HashMap, fs};
 
-// NOTES
-// * all components are objects
-// * some don't have the type field, for some reason
+fn organize_operation(
+    groups: &mut HashMap<String, HashMap<String, Operation>>,
+    operation_kind: &str,
+    path: &str,
+    operation: Operation,
+) {
+    let operation_and_path = format!("{operation_kind} {path}");
+
+    if let Some(group) = groups.get_mut(&operation.meta.group) {
+        group.insert(operation_and_path, operation);
+    } else {
+        groups.insert(
+            operation.meta.group.clone(),
+            [(operation_and_path, operation)].into(),
+        );
+    }
+}
 
 #[proc_macro]
 pub fn generate_from_schema(_item: TokenStream) -> TokenStream {
+    let mut result = String::new();
+
     let file = fs::read("openapi.yaml").expect("failed to read 'openapi.yaml'");
     let openapi_schema: OpenApi =
         serde_yaml::from_slice(&file).expect("failed to deserialize 'openapi.yaml'");
 
-    for (path, path_item) in openapi_schema.paths {
-        if let Some(operation) = path_item.get {
-            dbg!(path, operation.parameters);
+    let mut groups: HashMap<String, HashMap<String, Operation>> = HashMap::new();
+
+    for (path, item) in openapi_schema.paths {
+        if let Some(operation) = item.get {
+            organize_operation(&mut groups, "GET", &path, operation);
+        }
+
+        if let Some(operation) = item.post {
+            organize_operation(&mut groups, "POST", &path, operation);
+        }
+
+        if let Some(operation) = item.delete {
+            organize_operation(&mut groups, "DELETE", &path, operation);
         }
     }
 
-    // placeholder return value
-    _item
+    for group in groups {
+        // TODO: create modules for each group containing functions for their operations
+    }
+
+    dbg!(&result);
+
+    result.parse().unwrap()
 }
--- openai_proc_macros/src/openapi.rs
+++ openai_proc_macros/src/openapi.rs
@@ -5,72 +5,143 @@
 use serde::Deserialize;
 use std::collections::HashMap;
 
-#[derive(Deserialize, Debug)]
-pub struct OpenApi {
-    pub servers: Vec<Server>,
-    pub paths: HashMap<String, PathItem>,
-    pub components: Components,
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct OpenApi {
+    pub(crate) servers: Vec<Server>,
+    pub(crate) paths: HashMap<String, PathItem>,
+    pub(crate) components: Components,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct Server {
-    pub url: String,
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct Server {
+    pub(crate) url: String,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct PathItem {
-    pub get: Option<Operation>,
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct PathItem {
+    pub(crate) get: Option<Operation>,
+    pub(crate) post: Option<Operation>,
+    pub(crate) delete: Option<Operation>,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct Operation {
-    pub summary: String,
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct Operation {
+    pub(crate) summary: String,
     #[serde(rename = "operationId")]
-    pub operation_id: String,
+    pub(crate) operation_id: String,
     #[serde(default)]
-    pub parameters: Vec<Parameter>,
-}
-
-#[derive(Deserialize, Debug)]
-pub struct Parameter {
-    pub name: String,
-    pub r#in: String,
-    pub description: String,
-    pub required: bool,
+    pub(crate) parameters: Vec<Parameter>,
+    #[serde(rename = "requestBody")]
+    pub(crate) request_body: Option<RequestBody>,
+    pub(crate) responses: Responses,
     #[serde(default)]
-    pub deprecated: bool,
-    pub schema: Schema,
+    pub(crate) deprecated: bool,
+    #[serde(rename = "x-oaiMeta")]
+    pub(crate) meta: OpenAiMeta,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct Components {
-    pub schemas: HashMap<String, Schema>,
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct Parameter {
+    pub(crate) name: String,
+    pub(crate) r#in: String,
+    pub(crate) description: String,
+    pub(crate) required: bool,
+    #[serde(default)]
+    pub(crate) deprecated: bool,
+    pub(crate) schema: Schema,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct RequestBody {
+    pub(crate) description: Option<String>,
+    pub(crate) content: HashMap<String, MediaType>,
+    pub(crate) required: bool,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct MediaType {
+    pub(crate) schema: SchemaOrReference,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct Responses {
+    pub(crate) default: Option<Response>,
+    #[serde(flatten)]
+    pub(crate) statuses: HashMap<String, Response>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct Response {
+    pub(crate) description: String,
+    pub(crate) content: HashMap<String, Header>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct Header {
+    pub(crate) schema: SchemaOrReference,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct Reference {
+    #[serde(rename = "$ref")]
+    pub(crate) r#ref: String,
+}
+
+#[derive(Deserialize, Debug, Clone)]
 #[serde(untagged)]
-pub enum Schema {
-    Tagged(TaggedSchema),
-    Object(Object),
-    Other(serde_yaml::Value),
+pub(crate) enum SchemaOrReference {
+    Reference(Reference),
+    Schema(Schema),
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct Components {
+    pub(crate) schemas: HashMap<String, Schema>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(untagged)]
+pub(crate) enum Schema {
+    Tagged(TaggedSchema),
+    Object(ObjectSchema),
+}
+
+#[derive(Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 #[serde(tag = "type")]
-pub enum TaggedSchema {
+pub(crate) enum TaggedSchema {
     Null,
     Boolean,
-    Object(Object),
+    Object(ObjectSchema),
     Array,
     Number,
     String,
     Integer,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct Object {
-    pub properties: HashMap<String, Schema>,
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct ObjectSchema {
+    pub(crate) properties: Option<HashMap<String, Schema>>,
     #[serde(default)]
-    pub required: Vec<String>,
+    pub(crate) required: Vec<String>,
+    #[serde(rename = "x-oaiTypeLabel")]
+    pub(crate) type_label: Option<String>,
+    #[serde(default)]
+    pub(crate) nullable: bool,
+    pub(crate) description: Option<String>,
+    pub(crate) default: Option<serde_yaml::Value>,
+    pub(crate) title: Option<String>, // <-- last here
+    #[serde(flatten)]
+    pub(crate) other: serde_yaml::Value,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub(crate) struct OpenAiMeta {
+    pub(crate) name: String,
+    pub(crate) group: String,
+    pub(crate) path: String,
+    pub(crate) parameters: Option<String>,
+    pub(crate) response: Option<String>,
+    #[serde(default)]
+    pub(crate) beta: bool,
 }
